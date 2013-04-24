3f2795e887868617be6c3497d81b86c2a79943e4
bmullins <bmullins@techresidents.com>
2013-04-24 15:50:32 -0400

    Stub out index handler.

---
+++ indexsvc/handler.py
@@ -0,0 +1,140 @@
+import logging
+
+from sqlalchemy.sql import func
+
+from trpycore.factory.base import Factory
+from trpycore.pool.queue import QueuePool
+from trpycore.thread.util import join
+from trpycore.timezone import tz
+from trsvcscore.db.models import IndexJob as IndexJobModel
+from trsvcscore.service.handler.service import ServiceHandler
+from trindexsvc.gen import TIndexService
+from trindexsvc.gen.ttypes import NotificationPriority, UnavailableException, InvalidIndexDataException
+
+import settings
+
+from constants import NOTIFICATION_PRIORITY_VALUES
+from jobmonitor import IndexJobMonitor, IndexThreadPool
+from indexer import Indexer
+
+
+class IndexServiceHandler(TIndexService.Iface, ServiceHandler):
+    """IndexServiceHandler manages the index service.
+
+    This class specifies the service interface.
+    The handler base class provides functionality to register the
+    service with ZooKeeper, and sets up connections to our db using
+    SQLAlchemy.
+
+    """
+    def __init__(self, service):
+        super(IndexServiceHandler, self).__init__(
+		    service,
+            zookeeper_hosts=settings.ZOOKEEPER_HOSTS,
+            database_connection=settings.DATABASE_CONNECTION)
+
+        self.log = logging.getLogger("%s.%s" % (__name__, IndexServiceHandler.__name__))
+
+        # Create pool of Indexer objects which will do the
+        # actual work of indexing.
+        def indexer_factory():
+            return Indexer(
+                db_session_factory=self.get_database_session,
+                job_retry_seconds=settings.INDEXER_JOB_RETRY_SECONDS
+            )
+        self.indexer_pool = QueuePool(
+            size=settings.INDEXER_POOL_SIZE,
+            factory=Factory(indexer_factory))
+
+        # Create pool of threads to manage the work
+        self.thread_pool = IndexThreadPool(
+            num_threads=settings.INDEXER_THREADS,
+            indexer_pool=self.indexer_pool)
+
+        # Create job monitor which scans for new jobs
+        # to process and delegates to the thread pool
+        self.job_monitor = IndexJobMonitor(
+            db_session_factory=self.get_database_session,
+            thread_pool=self.thread_pool,
+            poll_seconds=settings.INDEXER_POLL_SECONDS)
+
+    def start(self):
+        """Start handler."""
+        super(IndexServiceHandler, self).start()
+        self.thread_pool.start()
+        self.job_monitor.start()
+
+    def stop(self):
+        """Stop handler."""
+        self.job_monitor.stop()
+        self.thread_pool.stop()
+        super(IndexServiceHandler, self).stop()
+
+    def join(self, timeout=None):
+        """Join handler."""
+        join([self.thread_pool, self.job_monitor, super(IndexServiceHandler, self)], timeout)
+
+
+    def _validate_index_params(self, context, data):
+        """Validate input params of the index() method
+        """
+        if not context:
+            raise InvalidIndexDataException('Invalid context')
+
+        # TODO validate data
+
+
+    # TODO not_before, priority support?
+    def index(self, context, data):
+        """Index data.
+
+        This method creates a job to index the provided data.
+
+        Args:
+            context: String to identify calling context
+            data: Thrift IndexData object.
+
+        Returns:
+            None
+        Raises:
+            InvalidNotificationException if input Notification
+            object is invalid.
+            UnavailableException for any other unexpected error.
+        """
+        try:
+
+            # Get a db session
+            db_session = self.get_database_session()
+
+            # Validate inputs
+            self._validate_index_params(context, data)
+
+            # If notification specified a start-processing-time
+            # convert it to UTC DateTime object.
+            # TODO is this a bug? I don't see a notBefore attribute for this object.
+            if notification.notBefore is not None:
+                processing_start_time = tz.timestamp_to_utc(notification.notBefore)
+            else:
+                processing_start_time = func.current_timestamp()
+
+            # Create NotificationJobs
+            job = IndexJobModel(
+                created=func.current_timestamp(),
+                not_before=func.processing_start_time,
+                priority=NOTIFICATION_PRIORITY_VALUES[
+                         NotificationPriority._VALUES_TO_NAMES[notification.priority]],
+                retries_remaining=settings.INDEXER_JOB_MAX_RETRY_ATTEMPTS
+            )
+            db_session.add(job)
+            db_session.commit()
+            return
+
+        except InvalidIndexDataException as error:
+            self.log.exception(error)
+            raise InvalidIndexDataException()
+        except Exception as error:
+            self.log.exception(error)
+            raise UnavailableException(str(error))
+
+        finally:
+            db_session.close()
